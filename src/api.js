23bf4a35a38ec90a3f325bd3a0689660a7a77891
gingerOk <oksanka.tarasyuk@gmail.com>
2020-11-07 18:49:51 +0200

    api

--- src/api.js
+++ src/api.js
@@ -1,23 +1,10 @@
 import axios from 'axios'
 const url = "https://my-json-server.typicode.com/gingerOk/zoo-project"
 
-// export default api = {
-//     animals: {
-//         fetchAll: () => axios.get(url).then(res => res.data.animals),
-//         create: animal =>
-//         axios.post(url, {animal}).then(res => res.data.animal),
-//       update: animal =>
-//         axios
-//           .put(`${url}${animal.id}`, {animal})
-//           .then(res => res.data.animal),
-//       delete: animal => axios.delete(`${url}${animal.id}`),
-//     },
-//     users: "",
-// }
-
-
 export const animals = {
     fetchAll: () => axios.get(`${url}/animals`).then(res => res.data),
     create: animal =>
     axios.post(url, {animal}).then(res => res.data.animal),
+    update: animal => axios.post(`${url}/animals${animal.id}`, {animal}).then(res => res.data),
+    delete: animal => axios.delete(`${url}/animals${animal.id}`),
 }
