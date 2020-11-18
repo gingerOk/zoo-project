2adb03c733c391f3756c2ac001abe60802d47d8f
gingerOk <oksanka.tarasyuk@gmail.com>
2020-11-18 23:15:28 +0200

    to do

--- src/_services/server.js
+++ src/_services/server.js
@@ -15,6 +15,8 @@ server.use(jsonServer.defaults());
 const SECRET_KEY = '123456789'
 const expiresIn = '1h'
 
+//Fetch animals from db.json
+// TO DO
 
 // Create a token from a payload 
 function createToken(payload){
